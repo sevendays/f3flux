1718aa5095c4fd60979e7083390b2eaf1b272120
seven <zamboluca@gmail.com>
2013-05-09 23:39:39 +0200

    Removed Auth and Cfg classes, added vanilla FluxBB support and license.
    It made more sense to have only one class -- Bridge.
    And the changes to support vanilla FluxBB were trivial.
    And license... Well, the one my editor put in. :)

--- FluxBridge/Bridge.php
+++ FluxBridge/Bridge.php
@@ -1,36 +1,316 @@
 <?php
 
+/*
+ * Bridge.php
+ * 
+ * Copyright 2013 Luca Zamboni <zamboluca@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
 namespace FluxBridge;
-use DB;
 
 class Bridge {
-	private $fluxbb_root = null;
 	
-	public $db = null; // F3 database instance
-	public $cfg = null; // FluxBB configs array
-	public $auth = null; // auth class
+	// FluxBB root dir
+	protected $fluxbb_root;
+	// is it vanilla FluxBB or the GameZoo one?
+	protected $vanilla_fluxbb;
+	// Holds connection to FluxBB database
+	public $db;
+	// FluxBB's $pun_config loaded from db
+	public $pun_config = null;
+	// FluxBB's user data loaded from db
+	public $pun_user = null;
+	// FluxBB's cookie config
+	private $_cookie = null;
+	// FluxBB's db config
+	private $_db = null;
 	
-	function __construct($fluxbb_root)
+	private $loggedIn = false;
+	
+	/* functions:
+	1- auth user
+	2- retrieve user from id
+	3- retrieve user from username
+	*/
+	
+	// authenticates the current user via the cookie
+	// returns true or false
+	// populates pun_user
+	public function isLoggedIn()
 	{
-		$this->fluxbb_root = $fluxbb_root; // TODO check validl
+		// if that's set to true, we've already passed the check during this request.
+		if($this->loggedIn)
+			return true;
 		
-		// read config.php
-		$this->cfg = new Cfg($fluxbb_root.'config.php');
+		$now = time();
+
+		// If the cookie is set and it matches the correct pattern, then read the values from it
+		if (isset($_COOKIE[$this->_cookie['name']]) && preg_match('%^(\d+)\|([0-9a-fA-F]+)\|(\d+)\|([0-9a-fA-F]+)$%', $_COOKIE[$this->_cookie['name']], $matches))
+		{
+			$cookie = array(
+				'user_id'			=> intval($matches[1]),
+				'password_hash' 	=> $matches[2],
+				'expiration_time'	=> intval($matches[3]),
+				'cookie_hash'		=> $matches[4],
+			);
+		}
+		else
+		{
+			$this->pun_user = null;
+			$this->loggedIn = false;
+			return false;
+		}
+
+		// If it has a non-guest user, and hasn't expired
+		if (isset($cookie) && $cookie['user_id'] > 1 && $cookie['expiration_time'] > $now)
+		{
+			// If the cookie has been tampered with
+			if ($this->forum_hmac($cookie['user_id'].'|'.$cookie['expiration_time'], $this->_cookie['seed'].'_cookie_hash') != $cookie['cookie_hash'])
+			{
+				$expire = $now - 3600; // delete the cookie
+				$this->pun_setcookie(1, $this->pun_hash(uniqid(rand(), true)), $expire);
+				$this->pun_user = null;
+				$this->loggedIn = false;
+				return false;
+			}
+
+			// Check if there's a user with the user ID and password hash from the cookie
+			// retrieve and save user infos
+			$result = $this->db->exec('SELECT * FROM '.$this->_db['prefix'].'users WHERE id='.$cookie['user_id']);
+			if($this->db->count() == 0) // no such name
+			{
+				$expire = $now - 3600; // delete the cookie
+				$this->pun_setcookie(1, $this->pun_hash(uniqid(rand(), true)), $expire);
+				$this->pun_user = null;
+				$this->loggedIn = false;
+				return false;
+			}
+			$this->pun_user = $result[0];
+			
+			// If user authorisation failed
+			if (!isset($this->pun_user['id']) || $this->forum_hmac($this->pun_user['password'], $this->_cookie['seed'].'_password_hash') !== $cookie['password_hash'])
+			{
+				$expire = $now - 3600; // delete the cookie
+				$this->pun_setcookie(1, $this->pun_hash(uniqid(rand(), true)), $expire);
+				$this->pun_user = null;
+				$this->loggedIn = false;
+				return false;
+			}
+
+			// Send a new, updated cookie with a new expiration timestamp
+			$expire = ($cookie['expiration_time'] > $now + $this->pun_config['o_timeout_visit']) ? $now + 1209600 : $now + $this->pun_config['o_timeout_visit'];
+			$this->pun_setcookie($this->pun_user['id'], $this->pun_user['password'], $expire);
+			
+			$this->loggedIn = true;
+			return true;
+		}
+		else
+		{
+			$this->pun_user = null;
+			$this->loggedIn = false;
+			return false;
+		}
+	}
+	
+	// sets fluxbb cookie
+	// returns true on success, false otherwise
+	// populates pun_user
+	public function login($username, $password, $save_pass = false)
+	{
+		if(empty($username) or empty ($password))
+			return false;
 		
-		// establish database connection.
-		$this->db = new DB\SQL(
-			'mysql:host='.$this->cfg->db['host'].';dbname='.$this->cfg->db['name'],
-			$this->cfg->db['username'],
-			$this->cfg->db['password']
-		);
+		if($this->loggedIn)
+			return true;
 		
-		// read forum config from database
-		$this->cfg->readBoardConfig($this->db);
+		// retrieve user infos
+		$result = $this->db->exec('SELECT * FROM '.$this->_db['prefix'].'users WHERE username=?', $username);
+		if($this->db->count() == 0)
+		{
+			$this->pun_user = null;
+			$this->loggedIn = false;
+			return false;
+		}
+		$this->pun_user = $result[0];
 		
-		// initialize Auth methods
-		$this->auth = new Auth($this->db, $this->cfg);
+		$authorized = false;
 		
-		// 
+		// GameZoo login method using PHPass
+		if(!$this->vanilla_fluxbb)
+		{
+			$hasher = new \PasswordHash(8, false);
+			$authorized = $hasher->CheckPassword($password, $this->pun_user['password']);
+		}
+		else // vanilla FluxBB login method
+		{
+			// copying our variables to FLuxBB's code (login.php)
+			$form_password = $password;
+			$cur_user = $this->pun_user;
+			
+			// BEGIN c&p from login.php
+			$form_password_hash = pun_hash($form_password); // Will result in a SHA-1 hash
+			
+			// If there is a salt in the database we have upgraded from 1.3-legacy though haven't yet logged in
+			if (!empty($cur_user['salt']))
+			{
+				if (sha1($cur_user['salt'].sha1($form_password)) == $cur_user['password']) // 1.3 used sha1(salt.sha1(pass))
+				{
+					$authorized = true;
+
+					//$db->query('UPDATE '.$db->prefix.'users SET password=\''.$form_password_hash.'\', salt=NULL WHERE id='.$cur_user['id']) or error('Unable to update user password', __FILE__, __LINE__, $db->error());
+					$this->db->exec('UPDATE'.$this->_db['prefix'].'users SET password=\''.$form_password_hash.'\', salt=NULL WHERE id='.$cur_user['id']) or trigger_error('Unable to update user password');
+				}
+			}
+			// If the length isn't 40 then the password isn't using sha1, so it must be md5 from 1.2
+			else if (strlen($cur_user['password']) != 40)
+			{
+				if (md5($form_password) == $cur_user['password'])
+				{
+					$authorized = true;
+
+					//$db->query('UPDATE '.$db->prefix.'users SET password=\''.$form_password_hash.'\' WHERE id='.$cur_user['id']) or error('Unable to update user password', __FILE__, __LINE__, $db->error());
+					$this->db->exec('UPDATE '.$this->_db['prefix'].'users SET password=\''.$form_password_hash.'\' WHERE id='.$cur_user['id']) or trigger_error('Unable to update user password');
+				}
+			}
+			// Otherwise we should have a normal sha1 password
+			else
+				$authorized = ($cur_user['password'] == $form_password_hash);
+			// END c&p from login.php
+		}
+		
+		// set cookie or say bye
+		if($authorized)
+		{
+			// Send a new, updated cookie with a new expiration timestamp
+			$expire = ($save_pass) ? time() + 1209600 : time() + $this->pun_config['o_timeout_visit'];
+			$this->pun_setcookie($this->pun_user['id'], $this->pun_user['password'], $expire);
+			$this->loggedIn = true;
+			return true;
+		}
+		else
+		{
+			$this->pun_user = null;
+			$this->loggedIn = false;
+			return false;
+		}
+	}
+	
+	// clears fluxbb cookie
+	// no return
+	public function logout()
+	{
+		if($this->loggedIn)
+		{
+			// remove cookie
+			$this->pun_setcookie($this->pun_user['id'], $this->pun_user['password'], time()-10);
+			$this->loggedIn = false;
+			$this->pun_user = null;
+		}
+	}
+	
+	// imported as-is (FluxBB 1.5.3)
+	private function forum_hmac($data, $key, $raw_output = false)
+	{
+		if (function_exists('hash_hmac'))
+			return hash_hmac('sha1', $data, $key, $raw_output);
+
+		// If key size more than blocksize then we hash it once
+		if (strlen($key) > 64)
+			$key = pack('H*', sha1($key)); // we have to use raw output here to match the standard
+
+		// Ensure we're padded to exactly one block boundary
+		$key = str_pad($key, 64, chr(0x00));
+
+		$hmac_opad = str_repeat(chr(0x5C), 64);
+		$hmac_ipad = str_repeat(chr(0x36), 64);
+
+		// Do inner and outer padding
+		for ($i = 0;$i < 64;$i++) {
+			$hmac_opad[$i] = $hmac_opad[$i] ^ $key[$i];
+			$hmac_ipad[$i] = $hmac_ipad[$i] ^ $key[$i];
+		}
+
+		// Finally, calculate the HMAC
+		$hash = sha1($hmac_opad.pack('H*', sha1($hmac_ipad.$data)));
+
+		// If we want raw output then we need to pack the final result
+		if ($raw_output)
+			$hash = pack('H*', $hash);
+
+		return $hash;
+	}
+	
+	// imported as-is (FluxBB 1.5.3)
+	private function pun_hash($str)
+	{
+		return sha1($str);
+	}
+	
+	// imported as-is (FluxBB 1.5.3) + slightly modified
+	private function pun_setcookie($user_id, $password_hash, $expire)
+	{
+		//global $cookie_name, $cookie_seed;
+		$this->forum_setcookie($this->_cookie['name'], $user_id.'|'.$this->forum_hmac($password_hash, $this->_cookie['seed'].'_password_hash').'|'.$expire.'|'.$this->forum_hmac($user_id.'|'.$expire, $this->_cookie['seed'].'_cookie_hash'), $expire);
+	}
+	
+	// imported as-is (FluxBB 1.5.3)
+	private function forum_setcookie($name, $value, $expire)
+	{
+		//global $cookie_path, $cookie_domain, $cookie_secure;
+		// Enable sending of a P3P header
+		header('P3P: CP="CUR ADM"');
+		if (version_compare(PHP_VERSION, '5.2.0', '>='))
+			setcookie($name, $value, $expire, $this->_cookie['path'], $this->_cookie['domain'], $this->_cookie['secure'], true);
+		else
+			setcookie($name, $value, $expire, $this->_cookie['path'].'; HttpOnly', $this->_cookie['domain'], $this->_cookie['secure']);
+	}
+	
+	function __construct($fluxbb_root, $is_vanilla)
+	{
+		if($is_vanilla === true) $this->vanilla_fluxbb = true;
+		
+		require_once($fluxbb_root."config.php");
+		$this->_db['host'] = $db_host;
+		$this->_db['name'] = $db_name;
+		$this->_db['username'] = $db_username;
+		$this->_db['password'] = $db_password;
+		$this->_db['prefix'] = $db_prefix;
+		$this->_cookie['name'] = $cookie_name;
+		$this->_cookie['domain'] = $cookie_domain;
+		$this->_cookie['path'] = $cookie_path;
+		$this->_cookie['secure'] = $cookie_secure;
+		$this->_cookie['seed'] = $cookie_seed;
+		
+		// open connection to FluxBB DB
+		$this->db = new \DB\SQL('mysql:host='.$db_host.';dbname='.$db_name, $db_username, $db_password);
+		
+		// retrieve pun_config from database
+		$result = $this->db->exec('SELECT * FROM '.$db_prefix.'config');
+		
+		foreach($result as $row)
+		{
+			$this->pun_config[$row['conf_name']] = $row['conf_value'];
+		}
+		
+		// set initial variables values
+		$this->loggedIn = false;
 	}
 }
-?>
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #f3flux
 
-Bridge between Fat-Free framework (https://github.com/bcosca/fatfree) and GameZoo FluxBB 1.5.* (https://github.com/sevendays/fluxbb15-GZ).
+Bridge between Fat-Free framework (https://github.com/bcosca/fatfree) and FluxBB 1.5.* (https://github.com/fluxbb/fluxbb).
 
 With this bridge you'll be able to authenticate (login, logout, isLogged) users using FluxBB cookies.
 
@@ -10,8 +10,8 @@ Or you have an user coming from the forum and you want to identify it -- now you
 
 ##Remarks:
 
-* This code does __not__ work with vanilla FluxBB 1.5.*  because it has been designed for GameZoo's implementation (using PHPass for passwords instead of SHA1). Feel free to fork this repository and edit the code to make it work with vanilla FluxBB.
-* You must have F3. This code has been developed with F3 ver. _3.0.2_.
+* Now working with vanilla FluxBB 1.5.*!
+* You must have F3. This code has been developed with F3 ver. _3.0.7_.
 * MySQL only. This code uses F3's DB\SQL 
 
 ##Installing:
@@ -23,17 +23,25 @@ Or you have an user coming from the forum and you want to identify it -- now you
 ##Usage:
 
 ### Domain-wide cookies
-If you have your site in a different subdomain than the forum (e.g.: the site is www.gamezoo.it, the forum is forum.gamezoo.it), you need to edit fluxbb's config.php:
+If you have your site in a different subdomain than the forum (e.g.: the site is www.gamezoo.it, the forum is forum.gamezoo.it), you need to edit fluxbb's `config.php`:
 
     $cookie_domain = 'gamezoo.it'; // replace with your domain!
     $cookie_path = '/';
 
 Read more about it here: http://php.net/manual/en/function.setcookie.php
 
-### Bridge creation
+A nice trick: if you're serving the same forum on more than one site (forum.gamezoo.it, forum.gamezoo.org) you can have this instead:
+    
+    /* since we've got more than one domain... */
+    preg_match("/[^\.\/]+\.[^\.\/]+$/", $_SERVER['HTTP_HOST'], $match);
+    $cookie_domain = $match[0];
+    unset($match);
+    $cookie_path = '/';
 
-You only need to pass the constructor the path of your FluxBB installation.
 
+### User authentication
+
+You only need to pass the constructor the path of your FluxBB installation. The bridge will connect to FluxBB's database and check/set the session cookie.
 
     // start using F3
     $f3 = require('/path/to/fat-free-framework/lib/base.php');
@@ -43,60 +51,43 @@ You only need to pass the constructor the path of your FluxBB installation.
     // define FluxBB path (don't forget the trailing slash)
     $f3->set('fluxbb_root', '/path/to/FluxBB/')
     
-    // create FluxBB bridge as global object
-    //  the class is being autoloaded by F3!
-    $f3->set('FluxBB', new FluxBridge\Bridge($f3->get('fluxbb_root')));
-
-The Bridge constructor will:
-* read config.php
-* connect to the database with FluxBB's credentials
-* read the forum configuration ($pun_config in FluxBB's code)
-
-### Reading FluxBB config
-
-The config is managed through the class __FluxBridge\Cfg__.
-
-Cookie/DB variables in fluxbb/config.php are stored in two arrays:
-
-    $cookie_config = $f3->get('FluxBB')->cfg->cookie;
-    $db_config = $f3->get('FluxBB')->cfg->db;
-
-The board configuration is stored in the pun_config array:
-
-    $pun_config = $f3->get('FluxBB')->cfg->pun_config;
-
-### Authenticating users
-
-The class __FluxBridge\Auth__ has the methods to:
-* check if the user is logged in
-* login the user
-* logout the user
-
-Example:
-
-    // check if the user is logged in
-    $logged_in = $f3->get('FluxBB')->auth->isLoggedIn();
+    // create FluxBB bridge
+    $bridge =  new FluxBridge\Bridge($f3->get('fluxbb_root'));
     
-    if($logged_in)
-    {
-        // log the user out
-        $f3->get('FluxBB')->auth->logout();
-    }
+    // see if the current user is logged in
+    $loggedIn = $bridge->isLoggedIn();
+    
+    //... if he's not logged in, log him in
+    if(!$loggedIn)
+        if ($bridge->login($username, $password, $remember))
+            echo "logged in";
+        else echo "wrong user/pass";
+    
+    //... if he's logged in, log him out
     else
-    {
-        // log the user in
-        // $form_user and $form_password come from the login form
-        $f3->get('FluxBB')->auth->login($form_username, $form_password);
-        
-        // this sets the cookie for a longer time than the one defined in $pun_config['o_timeout_visit']
-        //$f3->get('FluxBB')->auth->login($form_username, $form_password, TRUE);
-    }
+	$bridge->logout();
 
-### Reading current user ($pun_user)
 
-While the user is logged in you can read the user properties:
+### Useful objects
 
-    $pun_user = $f3->get('FluxBB')->auth->pun_user;
+After the bridge instantiation you've got a working DB\SQL connection to FluxBB's database:
+    // $bridge->db is the db connection
+    $bridge->db->exec('SELECT * FROM users LIMIT 10'); // hmmm no table prefix...
 
-This is equivalent to FluxBB pun_user variable: it holds the user's row found in FluxBB's 'users' table.
+Likewise, the  board configuration is stored in the `pun_config` array:
+    $cookie_timeout = $bridge->pun_config['o_timeout_visit'];
     
+If the user is logged in (after a successful `login()` or `isLoggedIn()`) you've got his properties in an array:
+    // $bridge->pun_user is the user array
+    $user_id = $bridge->pun_user['id'];
+    $username = $bridge->pun_user['username'];
+
+### Using the bridge with GameZoo's FluxBB (featuring PHPass and Akismet, https://github.com/sevendays/fluxbb15-GZ)
+
+Couldn't be simpler.
+
+* Grab PHPass from here: http://www.openwall.com/phpass/
+* Put the `PasswordHash.php` file in your f3's autoload folder
+* Instantiate the bridge with an additional parameter: `$bridge = new \FluxBridge\Bridge($fluxbb_root, false);`. The second parameter specifies that you're not using vanilla FluxBB.
+* Profit.
+
